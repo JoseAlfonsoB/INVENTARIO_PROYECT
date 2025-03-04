7a702c29bf5710364e2ffafab9057bb6f0c21e78
JoseAlfonsoB <jalfonsobautista74@gmail.com>
2025-03-04 12:26:39 -0600

    Función para sumar dos números

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -11,7 +11,8 @@ app.use(express.json()); // INTERPRETE DE JSON
 // app.use("/una_ruta",express.json()) SOLO SE VA EJECUTAR EN ESA RUTA
 app.use(cors()) // PERMITE O NIEGA EL ACCESO A LAS IPs
 
-// MÉTODO GET
+
+// MÉTODOS GET
 app.get("/", (req, res)/** PARA LA FUNCIÓN DE FLECHA: Lo que requiero y lo que devuelvo */ => {
     //res.send("<h1>Hola desde Express</h1>"); PARA MANDAR TEXTO PLANO.
     res.json (
@@ -30,6 +31,19 @@ app.get("/:nombre", (req, res) => {
             mensaje:"Hola usando una URL personal de: "+nombre
         }
     );
+});
+
+// PARA VERIFICAR QUE TODO LOS PARAMETROS DE LA SUMA SEAN NÚMEROS
+app.get("/:n1/:n2", (req, res) => {
+    console.log(req.params);
+    const {n1, n2} = req.params;
+    if (isNaN(parseFloat(n1)) || isNaN(parseFloat(n2))) {
+        res.status(400).json({message:"No todos los parámetros son números"})
+    }else {
+        const suma = parseInt(n1)+parseFloat(n2);
+        res.json({suma});
+        
+    }
 });
 
 // CORRIENDO EL SERVIDOR
