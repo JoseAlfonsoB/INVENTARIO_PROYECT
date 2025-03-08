d7a1af3e33d205c2f6a31791fd0c106965f90403
JoseAlfonsoB <jalfonsobautista74@gmail.com>
2025-03-07 20:27:07 -0600

    Se agregarón datos de prueba para verlos en el servidor.

--- BackEnd/DataBase/DB.js
+++ BackEnd/DataBase/DB.js
@@ -1,4 +1,23 @@
-const productos = [];
+const productos = [
+    {
+        id: 1,
+        nombre: "Producto 1", 
+        precio: 100,
+        stock: 100
+    },
+    {
+        id: 2,
+        nombre: "Producto 2",
+        precio: 200,
+        stock: 200
+    },
+    {
+        id: 3,
+        nombre: "Producto 3",
+        precio: 300,
+        stock: 300
+    },
+];
 
 /** FORMA EXPLICITA
 module.exports = {
